9ca9d190a5c227a25bfa0fa60ce71080f6ed489e
LTDnorman <amsrout@yandex.ru>
2020-02-16 16:53:29 +0300

    HelloWorld start2

--- src/package1/FastCODE.java
+++
@@ -1,9 +0,0 @@
-package package1;
-
-public class FastCODE {
-    public static void main(String[] args) {
-        System.out.println("psvm - public static void main...");
-        System.out.println("sout - System.out.println...");
-
-    }
-}
