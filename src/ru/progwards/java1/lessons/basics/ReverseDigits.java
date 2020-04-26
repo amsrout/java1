6d7f45e5b21ea1c009d9f1653f3383ed5a7f3ab5
LTDnorman <amsrout@yandex.ru>
2020-04-26 16:32:48 +0300

    HelloWorld start6

--- src/ru/progwards/java1/lessons/basics/ReverseDigits.java
+++ src/ru/progwards/java1/lessons/basics/ReverseDigits.java
@@ -1,14 +1,14 @@
 package ru.progwards.java1.lessons.basics;
 
 public class ReverseDigits {
-        public static int reverseDigits(int number) {
-            int a = number % 10; // остаток от деления 456 на 10 будет 6 - первое число
-            int b = number / 10 % 10; // 456 делим на 10 = 45. остаток от деления 45 на 10 будет 5 - второе число
-            int c = number / 100; // 456 делим на 100 = 4 - третье число
-            return a*100 + b*10 + c; // первое число умножаем на сотни, второе на десятки и складываем
-        }
-
-        public static void main(String[] args) {
-            System.out.println(reverseDigits(456));
-        }
+    public static int reverseDigits(int number) {
+        int a = number % 10;
+        int b = number / 10 % 10;
+        int c = number / 100;
+        return a*100 + b*10 + c;
     }
+
+    public static void main(String[] args) {
+        System.out.println(reverseDigits(853));
+    }
+}
